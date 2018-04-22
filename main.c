eef02da7853fa4b36bc45c437208e78718584597
RaphaelKrief <sachagopok@gmail.com>
2018-04-22 10:03:14 +0200

    add the function that check if room exist in path in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 13:57:17 by rkrief            #+#    #+#             */
-/*   Updated: 2018/04/20 14:10:11 by rkrief           ###   ########.fr       */
+/*   Updated: 2018/04/22 10:01:33 by Raphael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,19 @@ int     ft_check_ifexist(char *room, char *path)
 	j = 0;
 	while (path[i])
 	{
-		ft_putstr("hey");
-		while (room[j] || path[i] == '-')
-		{
-			if (room[j] != path[i])
+			if (room[j] == path[i])
 			{
-				while (path[i] || path[i] != '-')
+				while (path[i] != '-' && room[j] && room[j] == path[i])
+				{
+					j++;
 					i++;
-				break;
+				}
+				if (!room[j] && (path[i] == '-' || !path[i])) 
+					return (1);
 			}
-			j++;
-			i++;
-		}
-		if (!room[j] && path[i] == '-')
-			return (1);
-		j = 0;
+			j = 0;
+			while (path[i] && path[i] != '-')
+				i++;
 		i++;
 	}
 	return (0);
@@ -61,6 +59,6 @@ int     ft_check_ifexist(char *room, char *path)
 
 int main()
 {
-	printf("%d\n", ft_check_ifexist("0", "1-2-3-4-5-g-0-d"));
+	printf("%d\n", ft_check_ifexist("à", "1-2-3-4-5-t-2-à"));
 	return (0);
 }
