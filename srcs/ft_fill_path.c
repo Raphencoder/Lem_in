a7432d59e042d3e8572fb34d5f3dab0379f34d5d
RaphaelKrief <sachagopok@gmail.com>
2018-04-24 18:40:44 +0200

    test of map 01 works perfectly, apparently find all the ways on of the map

--- srcs/ft_fill_path.c
+++ srcs/ft_fill_path.c
@@ -6,39 +6,102 @@
 /*   By: rkrief <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 16:44:09 by rkrief            #+#    #+#             */
-/*   Updated: 2018/04/24 11:58:21 by Raphael          ###   ########.fr       */
+/*   Updated: 2018/04/24 18:08:04 by Raphael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lem_in.h"
 
 
-char	*ft_rm_last_one(char **path)
+int     ft_check_path(char *path, char **allpath)
 {
 	int i;
-	int j;
-	char *res;
-	
+
 	i = 0;
-	while (*path[i])
+	if (allpath == NULL)
+		return (0);
+	while (allpath[i])
+	{
+		if (ft_strequ(path, allpath[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char    *ft_get_last_inpath(char *path)
+{
+	int     i;
+	int     k;
+	int     j;
+	char    *res;
+
+	i = 0;
+	k = 0;
+	if (path == NULL)
+		return (NULL);
+	while (path[i])
 		i++;
 	j = i;
-	while (*path[i] != '-' || i >= 0)
+	while (path[i] != '-' && i > 0)
 		i--;
-	res = (char*)ft_memalloc(sizeof(char) * (j - 1 + 1));
-	j = 0;
 	if (i == 0)
-		return (NULL);
-	while (*path[i])
 	{
-		if (*path[i] != '-')
-			res[j++] = *path[i];	
-		*path[i] = '\0';
-		i++;
+		res = ft_strdup(path);
+		return (res);
+	}
+	res = (char*)ft_memalloc(sizeof(char) * (j - i));
+	i++;
+	while (path[i])
+	{
+		res[k++] = path[i++];
 	}
 	return (res);
 }
 
+void	ft_modify(char *path, int k)
+{
+	char	*tmp;
+	int	i;
+
+	i = 0;
+	tmp = ft_strdup(path);
+	ft_strclr(path);
+	while (i < k)
+	{
+		path[i] = tmp[i];
+		i++;
+	}
+}
+
+char    *ft_rm_last_one(char *path)
+{
+	int i;
+	int j;
+	int k;
+	char *res;
+
+	i = 0;
+	while (path[i])
+		i++;
+	i--;
+	k = i;
+	while (path[i] != '-' && i >= 0)
+		i--;
+	res = (char*)ft_memalloc(sizeof(char) * (k - i + 1));
+	k = i;
+	j = 0;
+	if (i == 0)
+		return (NULL);
+	while (path[i])
+	{
+		if (path[i] != '-')
+			res[j++] = path[i];
+		i++;
+	}
+	ft_modify(path, k);
+	return (res);
+}
 
 int     ft_check_ifexist(char *room, char *path)
 {
@@ -152,49 +215,90 @@ char	*ft_findlink(t_ants info, char **allpath)
 	char	*room;
 	char	*path;
 	char	*rm;
+	char	*rmi;
 	char	**tab_rm;
-	char	*clonepath;
+	char	**clonepath;
 	int		i;
+	int		k;
 	int		j;
+	int		t;
+	int		l;
+	int		m;
+	int		clonem;
 
 	i = 0;
+	m = 0;
 	j = 0;
-	tab_rm = (char**)ft_mememalloc(sizeof(char*) * (200));
-	clonepath = NULL;
-	allpath = 0;
+	l = 0;
+	rmi = NULL;
+	tab_rm = (char**)ft_memalloc(sizeof(char*) * (200));
+	clonepath = (char**)ft_memalloc(sizeof(char*) * (200));
+	rm = NULL;
 	room = info.start;
 	path = info.start;
 	if (!(nb_rooms = ft_find_nbroom(&info)))
 		return (0);
-	while (nb_rooms * 2)
+	k = nb_rooms * 5;
+	while (k)
 	{
 		while (info.tubes[i])
 		{
 			if (ft_find_room_intube(room, info.tubes[i]))
 			{
-				if (ft_check_ifexist(ft_get_room(room, info.tubes[i]), path) || ft_check_ifrm(ft_get_room(room, info.tubes[i++]), tab_rm))
-					continue ;
-				i--;	
-				path = ft_strjoin(ft_strjoin(path, "-"), ft_get_room(room, info.tubes[i]));
-				room = ft_get_room(room, info.tubes[i]);
-				if (ft_strequ(room, info.end))
-					return (path);
-				break ;
+				printf("i = %d\n", i);
+				t = ft_check_ifexist(ft_get_room(room, info.tubes[i]), path);
+				if (t)
+					i++;
+				else if (rmi && ft_find_room_intube(rmi, info.tubes[i]) && ft_find_room_intube(rm, info.tubes[i]))
+					i++;
+				else {
+					path = ft_strjoin(ft_strjoin(path, "-"), ft_get_room(room, info.tubes[i]));
+					printf("newpath = %s\n", path);
+					room = ft_get_room(room, info.tubes[i]);
+					if (ft_strequ(room, info.end))
+					{
+						if (ft_check_path(path, allpath))
+						{
+							rm = ft_rm_last_one(path);
+							room = ft_get_last_inpath(path);
+							rmi = ft_get_last_inpath(path);
+							i = 0;
+						}
+						else
+						{
+							k = nb_rooms * 10;
+							allpath[j] = ft_strdup(path);
+							printf("AJOUT AU TABLEAU\n %s\nAJOUT AU TABLEAU\n", allpath[j]);
+							rm = ft_rm_last_one(path);
+							room = ft_get_last_inpath(path);
+							rmi = ft_get_last_inpath(path);
+							i = 0;
+							j++;
+						}
+					}
+				}
 			}
-			i++;
+		i++;
 		}
 		i = 0;
-		nb_rooms--;
-		if (clonepath == NULL)
-			clonepath = path;
-		else if (ft_strequ(clonepath, path))
+		k--;
+		if (ft_check_path(path, clonepath))
 		{
-			rm = ft_rm_last_one(&path);
-			tab_rm[j] = rm;
-			j++;
+			m++;
+			clonem = m;
+			while (m)
+			{
+				rm = ft_rm_last_one(path);
+				m--;
+			}
+			m = clonem;
+			room = ft_get_last_inpath(path);
+			rmi = ft_get_last_inpath(path);
 		}
-		ft_putendl(path);
-		ft_putendl("hey");
+		else  
+		{
+			clonepath[l++] = ft_strdup(path);
+		}
 	}
 	return (path);
 }
@@ -207,7 +311,7 @@ char	*ft_find_path(t_ants info)
 	int		i;
 
 	i = 0;
-	allpath = (char**)ft_memalloc(sizeof(char*) * (info.nb_room + 1));
+	allpath = (char**)ft_memalloc(sizeof(char*) * (500 + 1));
 	//	while (1)
 	//	{
 	if (!(conexion = ft_findlink(info, allpath)))
@@ -236,5 +340,5 @@ void	ft_fill_path(t_ants info)
 	i = 0;
 	//	allpath=0;
 	ant_path = ft_createtab(info.nb_ant);
-	ft_putendl(ft_find_path(info));
+	ft_find_path(info);
 }
